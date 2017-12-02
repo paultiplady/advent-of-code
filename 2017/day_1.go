47cec6ad7ce96b31b64949fc5e8d46dd8705e71e
Paul Tiplady <paul@qwil.co>
2017-12-02 11:50:02 -0800

    Day 1 solution

---
+++ 2017/day_1.go
@@ -0,0 +1,30 @@
+package _017
+
+import (
+	"fmt"
+)
+
+func calculate(input string) int {
+	total := 0
+
+	for i, char := range input {
+		digit := int(char - '0')
+		fmt.Printf("Processing digit: %d\n", digit)
+
+		j := (i + 1) % len(input)
+		fmt.Printf("Next index: %d\n", j)
+		nextDigit := int(input[j] - '0')
+
+		fmt.Printf("Next digit: %d\n", nextDigit)
+
+		if digit == nextDigit {
+			fmt.Printf("Repeated digit, adding to total: %d\n", digit)
+			total += digit
+		}
+	}
+	return total
+}
+
+func main() {
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # advent-of-code
+
+Playing with Go and TDD to solve the Advent of Code problems.
+
